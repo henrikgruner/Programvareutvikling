2ee933007f28097b23c4def40a3c72585a715ec0
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-02-07 17:54:22 +0100

    Add basic requirements. Remove formatting test code

--- backend/auction/users/models.py
+++ backend/auction/users/models.py
@@ -1,8 +1,5 @@
-from django.apps import AppConfig
 from django.db import models
 
 
 class User(models.Model):
-    role = models.CharField(max_length=30)
-    start_date = models.DateField(null=True)
-    end_date = models.DateField()
+    pass
