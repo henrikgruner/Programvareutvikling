46cbb79ff17d459ac111af56d9f0f2ea424b3db2
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-02-07 16:47:54 +0100

    Add auto flake8 and black on pre-commit

--- backend/auction/users/models.py
+++ backend/auction/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    role = models.CharField(max_length=30)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField()
