13399ed4b100594e02abd4c4fe6d1de76fbd601a
@martkval <martkval@stud.ntnu.no>
2019-02-14 09:40:36 +0100

    #7

--- backend/auction/bids/models.py
+++ backend/auction/bids/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class AuctionItem(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=280)
+    end_date = models.DateField()
+    minimum_bid = models.IntegerField()
+    current_bid = models.IntegerField()
+    status = models.BooleanField(default=True)
+    author = models.ForeignKey(User, default=0, on_delete=models.CASCADE())
+    img = models.ImageField(default="default.jpg")
+    pickup_address = models.CharField(max_length=80)
