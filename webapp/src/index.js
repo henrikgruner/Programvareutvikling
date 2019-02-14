4c8d6e088f2ff93582e96933f0b9ff4a69df6224
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-02-14 12:47:24 +0100

    Set up pages and basic forms, clean up project

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./utils/global.css";
+import ScrollToTop from "./utils/scrollToTop";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Routes
+import FrontPage from "./routes/FrontPage";
+import ProfilePage from "./routes/ProfilePage";
+import LoginPage from "./routes/LoginPage";
+import SignUpPage from "./routes/SignUpPage";
+import NotFoundPage from "./routes/NotFoundPage";
+
+ReactDOM.render(
+  <Router>
+    <ScrollToTop>
+      <Switch>
+        <Route exact path="/" component={FrontPage} />
+        <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignUpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </ScrollToTop>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ webapp/src/routes/ProfilePage/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ProfilePage extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to the profile page</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
