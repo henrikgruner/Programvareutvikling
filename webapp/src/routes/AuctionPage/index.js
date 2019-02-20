3b1fc3338ba85d3828bdee78b890bab5ff3a0c60
viggosk <viggosk@stud.ntnu.no>
2019-02-20 14:43:30 +0100

    Refactored

--- webapp/src/routes/AuctionPage/index.js
+++ webapp/src/routes/AuctionPage/index.js
@@ -1,64 +1,117 @@
 import React, { Component } from "react";
+import { TextBoxField } from "../../components/form";
+import { SubmitButton } from "../../components/SubmitButton";
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
+import callApi from "../../utils/callApi";
 
-class AuctionPage extends Component {
-  render() {
-    return (
-      <div
-        className="App"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center"
-        }}
-      >
-        <div style={{ marginBottom: "50px" }}>
-          <span style={{ fontSize: "2em" }}>Øreplugger fra Grundig</span>
+const Test = ({ label, text }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: "auto"
+      }}
+    >
+      <span>{label}</span>
+      <span>{text}</span>
+    </div>
+  );
+};
+
+const AuctionForm = ({
+  touched,
+  error,
+  isSubmitting,
+  handleSubmit,
+  isValid
+}) => {
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
+      <div style={{ marginBottom: "50px" }}>
+        <span style={{ fontSize: "2em" }}>Øreplugger fra Grundig</span>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ marginRight: "20px" }}>
+          <img src="url" alt="Bilde" />
         </div>
 
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <div style={{ marginRight: "20px" }}>
-            <img src="url" alt="Bilde" />
+        <div>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Test label="Lederbud" text="110,-" />
+            <Test label="Slutter om" text="15 min 13 sek" />
           </div>
+          <div style={{ marginTop: "50px" }}>
+            <Form>
+              <Field name="bid" component={TextBoxField} />
+            </Form>
 
-          <div>
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  margin: "auto"
-                }}
-              >
-                <span>Lederbud</span>
-                <span>110,-</span>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  margin: "auto"
-                }}
-              >
-                <span>Slutter om</span>
-                <span>15 min 13 sek</span>
-              </div>
-            </div>
-            <div style={{ marginTop: "50px" }}>
-              <form>
-                <input type="text" />
-                <input type="submit" value="Gi bud" />
-              </form>
-            </div>
-            <div style={{ marginTop: "10px" }}>
-              <span>Budøkning: 15</span>
-            </div>
+            <SubmitButton
+              onClick={handleSubmit}
+              type="submit"
+              disabled={isSubmitting}
+              valid={isValid}
+            >
+              Log in
+            </SubmitButton>
+          </div>
+          <div style={{ marginTop: "10px" }}>
+            <span>Budøkning: 15</span>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+const AuctionPage = withFormik({
+  displayName: "AuctionForm",
+  validateOnChange: true,
+  enableReinitialize: true,
+
+  mapPropsToValues() {
+    return {
+      bid: ""
+    };
+  },
+
+  // What happens when you submit the form
+  handleSubmit(values, { setSubmitting }) {
+    var submission = {
+      bid: values.bid
+    };
+
+    return callApi("/login", {
+      method: "POST",
+      body: JSON.stringify(submission)
+    })
+      .then(() => {
+        setSubmitting(false);
+      })
+      .catch(err => {
+        alert("Det skjedde en feil.... ");
+        setSubmitting(false);
+        throw err;
+      });
+  },
+
+  // Validation of the form
+  validationSchema: Yup.object().shape({
+    bid: Yup.number("Skriv inn et bud")
+      .required("Skriv inn et bud")
+      .positive("Budet må være positivt")
+      .integer("Budet må være et heltall")
+  })
+})(AuctionForm);
 
 export default AuctionPage;
