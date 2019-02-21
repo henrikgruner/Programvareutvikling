d027068fd4e83990568b49d4753e8d775aa82dbe
@martkval <martkval@stud.ntnu.no>
2019-02-21 13:14:48 +0100

    Models fixes

--- backend/auction/bids/models.py
+++ backend/auction/bids/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Bid(models.Model):
+
+    reg_date = models.DateTimeField(auto_now_add=True)
+    amount = models.IntegerField()
+    author = models.ForeignKey(User, default=0, on_delete=models.CASCADE)
+
+
 class AuctionItem(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=280)
@@ -12,15 +19,9 @@ class AuctionItem(models.Model):
     minimum_bid = models.IntegerField()
     is_active = models.BooleanField(default=True)
     author = models.ForeignKey(User, default=0, on_delete=models.CASCADE)
-    img = models.ImageField(default=None)
+    img = models.FileField(default=None)
     pickup_address = models.CharField(max_length=80)
-    bids = models.OneToManyField()
-    winner = models.ForeignKey(User, default=0, on_delete=models.CASCADE)
-
-
-class Bid(models.Model):
-
-    reg_date = models.DateField(default=datetime.now())
-    amount = models.IntegerField()
-    author = models.ForeignKey(User, default=0, on_delete=models.CASCADE)
-    auction = models.ForeignKey(AuctionItem, default=0, on_delete=models.CASCADE)
+    bids = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    winner = models.ForeignKey(
+        User, default=0, on_delete=models.CASCADE, related_name="bidder"
+    )
