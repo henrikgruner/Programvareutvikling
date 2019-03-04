81dc8cd42b1c7325cd58c5cd4a524a2061b5ad93
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-03-04 13:11:45 +0100

    Seperate code from index.js into App.js

---
+++ webapp/src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components/macro";
+import ScrollToTop from "./utils/scrollToTop";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Routes
+import FrontPage from "./routes/FrontPage";
+import ProfilePage from "./routes/ProfilePage";
+import LoginPage from "./routes/LoginPage";
+import SignUpPage from "./routes/SignUpPage";
+import AuctionPage from "./routes/AuctionPage";
+import NotFoundPage from "./routes/NotFoundPage";
+import CreateAuctionPage from "./routes/CreateAuctionPage";
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+  align-items: center;
+`;
+
+export const ContentWrapper = styled.div`
+  max-width: var(--page-max-width);
+  margin: 0 auto;
+  width: 100%;
+  flex: 1 0 auto;
+`;
+
+const App = () => (
+  <Router>
+    <ScrollToTop>
+      <PageWrapper>
+        <Header />
+        <ContentWrapper>
+          <Switch>
+            <Route exact path="/" component={FrontPage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/signup" component={SignUpPage} />
+            <Route
+              path="/auctions"
+              render={({ match: { url } }) => (
+                <Switch>
+                  <Route exact path={`${url}/`} component={FrontPage} />
+                  <Route
+                    exact
+                    path={`${url}/new`}
+                    component={CreateAuctionPage}
+                  />
+                  <Route
+                    exact
+                    path={`${url}/:auctionId`}
+                    component={AuctionPage}
+                  />
+                </Switch>
+              )}
+            />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </ContentWrapper>
+        <Footer />
+      </PageWrapper>
+    </ScrollToTop>
+  </Router>
+);
+
+export default App;
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,82 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styled from "styled-components/macro";
 import { PersistGate } from "redux-persist/integration/react";
-
-import "./utils/global.css";
-import ScrollToTop from "./utils/scrollToTop";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-import store from "./store/configureStore";
 import { Provider } from "react-redux";
 
-// Routes
-import FrontPage from "./routes/FrontPage";
-import ProfilePage from "./routes/ProfilePage";
-import LoginPage from "./routes/LoginPage";
-import SignUpPage from "./routes/SignUpPage";
-import AuctionPage from "./routes/AuctionPage";
-import NotFoundPage from "./routes/NotFoundPage";
-import CreateAuctionPage from "./routes/CreateAuctionPage";
+import App from "./App";
+import "./utils/global.css";
 
+import store from "./store/configureStore";
 const redux = store();
-
-export const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  overflow: hidden;
-  align-items: center;
-`;
-
-export const ContentWrapper = styled.div`
-  max-width: var(--page-max-width);
-  margin: 0 auto;
-  width: 100%;
-  flex: 1 0 auto;
-`;
 
 ReactDOM.render(
   <Provider store={redux.store}>
     <PersistGate persistor={redux.persistor}>
-      <Router>
-        <ScrollToTop>
-          <PageWrapper>
-            <Header />
-            <ContentWrapper>
-              <Switch>
-                <Route exact path="/" component={FrontPage} />
-                <Route exact path="/profile" component={ProfilePage} />
-                <Route exact path="/login" component={LoginPage} />
-                <Route exact path="/signup" component={SignUpPage} />
-                <Route
-                  path="/auctions"
-                  render={({ match: { url } }) => (
-                    <Switch>
-                      <Route exact path={`${url}/`} component={FrontPage} />
-                      <Route
-                        exact
-                        path={`${url}/new`}
-                        component={CreateAuctionPage}
-                      />
-                      <Route
-                        exact
-                        path={`${url}/:auctionId`}
-                        component={AuctionPage}
-                      />
-                    </Switch>
-                  )}
-                />
-                <Route component={NotFoundPage} />
-              </Switch>
-            </ContentWrapper>
-            <Footer />
-          </PageWrapper>
-        </ScrollToTop>
-      </Router>
+      <App />
     </PersistGate>
   </Provider>,
   document.getElementById("root")
