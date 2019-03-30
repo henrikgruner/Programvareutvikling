610076adf03bc3810023947a443419ba35ca42a5
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-03-30 10:03:26 +0100

    Fixes #73 Add AuctionImage, Bid and UserProfile to admin panel

--- backend/auction/auctions/admin.py
+++ backend/auction/auctions/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Auction
+from .models import Auction, AuctionImage, Bid
 
 admin.site.register(Auction)
+admin.site.register(AuctionImage)
+admin.site.register(Bid)
